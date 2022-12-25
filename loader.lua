f9d17d26c51300effa13b8365d94c607b32a3d46
Alter <69980607+AlterRainbow@users.noreply.github.com>
2022-12-25 05:37:10 +0100

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+local id = game.PlaceId
+
+local games = {
+    [6422372837] = "https://raw.githubusercontent.com/AlterRainbow/script-loader/main/Snipers%20%5BBETA%5D.lua"
+}
+
+loadstring(game:HttpGet(games[id]))()
